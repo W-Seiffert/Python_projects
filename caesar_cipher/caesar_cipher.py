cf8a1b9971286465f44bb8a7daeebb6cef67abc7
W-Seiffert <65399980+W-Seiffert@users.noreply.github.com>
2021-02-27 19:07:28 +0100

    Update caesar_cipher.py

--- caesar_cipher/caesar_cipher.py
+++ caesar_cipher/caesar_cipher.py
@@ -16,8 +16,8 @@ import os
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
             'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 
-			'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 
-			'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+	    'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 
+	    'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
 def caesar(start_text, shift_amount, cipher_direction):
