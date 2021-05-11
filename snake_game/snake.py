3f9d9a8997b0c0078de74dcacb604da8b9c2b208
W-Seiffert <65399980+W-Seiffert@users.noreply.github.com>
2021-05-11 13:17:50 +0200

    Update snake.py

--- snake_game/snake.py
+++ snake_game/snake.py
@@ -11,22 +11,27 @@ RIGHT = 0
 class Snake:
 
     def __init__(self):
+        """ constructor of a snake object """
         self.segments = []
         self.create_snake()
         self.head = self.segments[0]
 
     def create_snake(self):
+        """ method to put together the segments of a snake object """
         for position in STARTING_POSITIONS:
             self.add_segment(position)
 
     def add_segment(self, position):
+        """ method to create a new snake segment and add it to the current segments list """
         new_segment = Turtle("square")
         new_segment.color("white")
         new_segment.penup()
         new_segment.goto(position)
         self.segments.append(new_segment)
 
-    def reset(self):                 # method to initialize the snake again (after a restart of the game)
+    def reset(self):
+        """ method to initialize another snake object if a round of the game has ended
+        because of the snake colliding with a wall or with its own tail """
         for seg in self.segments:
             seg.goto(1000, 1000)     # move all segments of the current snake outside the screen
         self.segments.clear()
@@ -34,9 +39,12 @@ class Snake:
         self.head = self.segments[0]
         
     def extend(self):
+        """ method to make a snake object extend (by adding another segment) if the snake
+        has collected a food """
         self.add_segment(self.segments[-1].position())    # position of the last segment
 
     def move(self):
+        """ method to move a snake object forward by 20 pixels """
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
@@ -44,17 +52,21 @@ class Snake:
         self.head.forward(MOVE_DISTANCE)
 
     def up(self):
+        """ method to set the snake's direction of movement to 'up' (~ North) """
         if self.head.heading() != DOWN:
             self.head.setheading(UP)
 
     def down(self):
+        """ method to set the snake's direction of movement to 'down' (~ South) """
         if self.head.heading() != UP:
             self.head.setheading(DOWN)
 
     def left(self):
+        """ method to set the snake's direction of movement to 'left' (~ West) """
         if self.head.heading() != RIGHT:
             self.head.setheading(LEFT)
 
     def right(self):
+        """ method to set the snake's direction of movement to 'right' (~ East) """
         if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
