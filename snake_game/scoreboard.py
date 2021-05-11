b304e578a5ad4a58390126b520982124da5cd044
W-Seiffert <65399980+W-Seiffert@users.noreply.github.com>
2021-05-11 14:30:55 +0200

    Update scoreboard.py

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -7,6 +7,7 @@ FONT = ("Courier", 12, "normal")
 class Scoreboard(Turtle):
 
     def __init__(self):
+        """ constructor of a scoreboard object """
         super().__init__()
         self.score = 0
         with open("data.txt") as data:
@@ -18,10 +19,13 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
+        """ method to update the scoreboard when starting or restarting the game """
         self.clear()
         self.write(f"Score: {self.score}, High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def reset(self):
+        """ method to reset the scoreboard if a round of the game has ended 
+        because of the snake colliding with a wall or with its own tail """
         if self.score > self.high_score:
             self.high_score = self.score
             with open("data.txt", mode="w") as data:
@@ -30,6 +34,7 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def increase_score(self):
+        """ method to increase the score if the snake has collected a food """
         self.score += 1
         self.update_scoreboard()
 
